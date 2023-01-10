28fac9fbd8dc07cc903ec108ffa2f3ef3e979546
Amila Welihinda <amilajack@gmail.com>
2023-01-09 17:12:22 -0800

    chore: bump palette

--- apps/www/utils/palette.ts
+++ apps/www/utils/palette.ts
@@ -1,14 +1,21 @@
-import { events, frames, init, measure, network, profiler, vitals } from '@palette.dev/browser'
+import {
+  init,
+  events,
+  paint,
+  measure,
+  network,
+  profiler,
+  vitals,
+  debounce,
+} from '@palette.dev/browser'
 import { useEffect, useRef } from 'react'
 
 init({
   key: 'cl9osspo10001jw089njbjsw4',
   // Collect ui, network, performance events, and profiles
-  plugins: [events(), network(), profiler(), measure(), frames()],
+  plugins: [events(), network(), profiler(), measure(), paint(), vitals()],
   version: process.env.NEXT_PUBLIC_COMMIT_SHA,
 })
-
-let interactionProfilingStarted = false
 
 // Profile page load
 //   * Sample every 10ms
@@ -17,40 +24,15 @@ if (typeof window !== 'undefined') {
   profiler.start({ sampleInterval: 10, maxBufferSize: 100_000 })
   addEventListener('load', () => {
     performance.measure('load')
-    setTimeout(() => {
-      if (!interactionProfilingStarted) profiler.stop()
-    }, 1_000)
   })
-}
-
-// A debounce util for profiling and labeling
-const debounce = (start: () => void, stop: () => void) => {
-  let timeoutId: ReturnType<typeof setTimeout>
-  return () => {
-    if (typeof timeoutId === 'number') {
-      clearTimeout(timeoutId)
-    } else {
-      start()
-    }
-    timeoutId = setTimeout(() => {
-      stop()
-      timeoutId = undefined
-    }, 1_000)
-  }
 }
 
 // Debounce starting and stopping the profiler
 export const usePalette = () => {
   const debounceProfiler = useRef(
     debounce(
-      () => {
-        interactionProfilingStarted = true
-        profiler.start({ sampleInterval: 10, maxBufferSize: 100_000 })
-      },
-      () => {
-        interactionProfilingStarted = false
-        profiler.stop()
-      }
+      () => profiler.stop(),
+      () => profiler.start({ sampleInterval: 10, maxBufferSize: 100_000 })
     )
   )
 

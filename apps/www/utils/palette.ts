6243913b2d596946aef5fc672f6ae4ad1e89910e
Amila Welihinda <amilajack@gmail.com>
2023-07-25 15:36:44 -0700

    fix: @palette.dev/browser@1.18.12-fix-client-paint-interaction-limit.11

--- apps/www/utils/palette.ts
+++ apps/www/utils/palette.ts
@@ -18,52 +18,19 @@ init({
   debug: true,
 })
 
-// Profile page load
-//   * Sample every 10ms
-//   * Start the profiler immediately
-if (typeof window !== 'undefined') {
-  profiler.start({ sampleInterval: 10, maxBufferSize: 100_000 })
-  addEventListener('load', () => {
-    performance.measure('load')
-  })
-}
-
-// Debounce starting and stopping the profiler
-export const usePalette = () => {
-  const debounceProfiler = useRef(
-    debounce(
-      () => profiler.stop(),
-      () => profiler.start({ sampleInterval: 10, maxBufferSize: 100_000 })
-    )
-  )
-
-  useEffect(() => {
-    const debounceProfilerRef = debounceProfiler.current
-
-    // -------------------------------------------------------------------
-    // Profile page interactions
-    //   * Collect samples every 10ms
-    //   * Start the profiler on click, keypress, mousemove, and wheel events
-    //   * Stop the profiler after 1s of inactivity
-    // -------------------------------------------------------------------
-    addEventListener('click', debounceProfilerRef, {
-      capture: true,
-    })
-    addEventListener('keypress', debounceProfilerRef, {
-      capture: true,
-    })
-    addEventListener('mousemove', debounceProfilerRef, {
-      capture: true,
-    })
-    addEventListener('wheel', debounceProfilerRef, {
-      capture: true,
-    })
-
-    return () => {
-      removeEventListener('click', debounceProfilerRef)
-      removeEventListener('keypress', debounceProfilerRef)
-      removeEventListener('mousemove', debounceProfilerRef)
-      removeEventListener('wheel', debounceProfilerRef)
-    }
-  }, [])
-}
+// Start the profiler on the following events:
+profiler.on(
+  [
+    'paint.click',
+    'paint.keydown',
+    'paint.scroll',
+    'paint.mousemove',
+    'markers.measure',
+    'events.load',
+    'events.dcl',
+  ],
+  {
+    sampleInterval: 1,
+    maxBufferSize: 100_000,
+  }
+)
